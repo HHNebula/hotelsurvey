0f045ff7803d2c90a53ddb8d157cc550bcc3c03f
HHNebula <sungsoomr@icloud.com>
2022-12-28 23:30:45 +0900

    Update

--- src/main/java/com/hotel/survey/hotelsurvey/survlets/admins/AdminPage.java
+++ src/main/java/com/hotel/survey/hotelsurvey/survlets/admins/AdminPage.java
@@ -32,29 +32,10 @@ public class AdminPage extends HttpServlet {
 
         // 3. 유효한 로그인 정보인지 검사
         SurveyWithDB surveyWithDB = new SurveyWithDB();
-        try {
-            // DB에 저장된 Admin 정보를 가져옴
-            ArrayList<HashMap> adminInfo = surveyWithDB.getAdminInfo();
-
-            // 세션에 저장된 정보와 DB에 저장된 로그인 정보를 비교함
-            for (int i = 0; i < adminInfo.size(); i++) {
-
-                // 어드민 정보를 하나 가져와서
-                HashMap<String, String> admin = adminInfo.get(i);
-
-                // 일치하는지 비교
-                if (admin.get("ID").equals(adminId) && admin.get("PW").equals(adminPw)) {
-
-                    // 일치하는 정보를 찾으면 로그인을 건너뛰고 반복문 종료
-                    path = "jsp/admin/main.jsp";
-                    break;
-                }
-                // 불일치한다면 로그인 페이지로 이동
-                path = "jsp/admin/login.jsp";
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        if (surveyWithDB.isAdmin(adminId, adminPw)) {
+            path = "jsp/admin/main.jsp";
+        } else {
+            path = "jsp/admin/login.jsp";
         }
 
         // 4. 저장된 경로로 랜딩
