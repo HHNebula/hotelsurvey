5e615d0e78d80892eadcc6a74d5e26575d5b7b45
HHNebula <sungsoomr@icloud.com>
2022-12-28 23:35:32 +0900

    Update

--- src/main/java/com/hotel/survey/hotelsurvey/survlets/admins/AdminLogin.java
+++ src/main/java/com/hotel/survey/hotelsurvey/survlets/admins/AdminLogin.java
@@ -2,7 +2,7 @@ package com.hotel.survey.hotelsurvey.survlets.admins;
 
 import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet(urlPatterns = "/admin/login")
+// @WebServlet(urlPatterns = "/admin/login")
 public class AdminLogin {
 
 }
