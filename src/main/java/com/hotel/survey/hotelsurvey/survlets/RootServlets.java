dec63ba4856cfaccc109f805faa437b9fcac4c33
현또라 <108624538+HyeonTtora@users.noreply.github.com>
2022-12-29 15:18:42 +0900

    httora

---
+++ src/main/java/com/hotel/survey/hotelsurvey/survlets/RootServlets.java
@@ -0,0 +1,24 @@
+package com.hotel.survey.hotelsurvey.survlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RootServlets", urlPatterns = "")
+public class RootServlets extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
