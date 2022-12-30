02a0cf561691ea0344f94f0d4d2d483560f1ac50
Ryuyeonjoo <fuswn87@gmail.com>
2022-12-30 15:22:59 +0900

    signup commit

--- src/main/java/com/hotel/survey/hotelsurvey/survlets/Signup.java
+++ src/main/java/com/hotel/survey/hotelsurvey/survlets/Signup.java
@@ -17,27 +17,32 @@ public class Signup extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // 1. 사용자가 입력한 로그인 정보를 가져옴
-        String email = request.getParameter("userEmail");
-        String password = request.getParameter("userPassword");
-
-        // 2. DB 조회
-        SurveyWithDB surveyWithDB = new SurveyWithDB();
+        // 1. 세션을 가져와 로그인 여부 체크
         HttpSession session = request.getSession();
-        String userId = surveyWithDB.isUser(email, password);
+
+        // 2. 세션의 userId 가져옴
+        String userId = (String) session.getAttribute("userId");
         
 
-         // 3. 받아온 USER_ID 값에 따라 랜딩
-         if (userId.equals("error") || userId.equals("null")) {
-            // 회원 정보가 없을 경우 - 에러 메세지 받아서 넘김
-            session.setAttribute("error", "일치하는 회원 정보가 없습니다.");
-            response.sendRedirect("/signup");
-        } else {
-            // 정보가 일치할 경우 - userId를 받아서 넘김
-            session.setAttribute("userId", userId);
-            response.sendRedirect("/home");
-        }
-
+         // 3. request.getAttribute("login"), false 면 signup.jsp 호출
+         // request.getAttribute("login"), true 면 /home 서블릿으로 보냄
+         
+         String path = null;
+         if(userId != null) {
+            switch(userId) {
+                case "error":
+                request.setAttribute("login", false);
+                break;
+            default:
+                request.setAttribute("login", true);
+                break;
+            }
+                response.sendRedirect("/home");
+            }else {
+                request.setAttribute("login", false);
+                 path = "jsp/prod/signup.jsp";
+            }
+            
     }
 
 }
